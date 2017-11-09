48a067468a291a4bb55fa49f602d81fdd246b922
Астафьев Андрей <a.astafev@bars-open.ru>
2017-11-09 15:40:10 +0300

    commit

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
+    @Input()
+    car:{name:string,year:number};
 
     constructor() { }
 
