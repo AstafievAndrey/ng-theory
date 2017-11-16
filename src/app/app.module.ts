def38782521f4659d4a64c851ca1bf3caa4f87d3
AstafievAndrey <a.astafev@bars-open.ru>
2017-11-16 16:24:30 +0300

    block4_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BackgroundDirective} from "./directives/background.directive";
+import { MyDirectiveDirective } from './directives/my-directive.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        BackgroundDirective,
+        MyDirectiveDirective
+    ],
   imports: [
     BrowserModule
   ],
