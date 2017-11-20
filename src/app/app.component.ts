60ddbdea912a65c5aa12eb2c7f4c9871f0d6bcf0
AstafievAndrey <a.astafev@bars-open.ru>
2017-11-20 17:36:02 +0300

    block5_3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    searchCar:string = '';
+    cars = [
+        {name:'Ford',description:'test1'},
+        {name:'Mystang',description:'test2'},
+        {name:'Mersedez',description:'test3'},
+        {name:'Bmv',description:'test4'},
+    ];
 }
---
+++ src/app/car-filter.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'carFilter'
+})
+export class CarFilterPipe implements PipeTransform {
+
+    transform(carList: any, searchStr: string, fieldName:string): any {
+        if(carList.length === 0 || searchStr === '' ){
+            return carList;
+        }
+        return carList.filter( car => car[fieldName].toLowerCase().indexOf(searchStr.toLowerCase()) !== -1 );
+    }
+}
