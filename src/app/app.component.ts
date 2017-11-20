4a31feb36a3bde71493a425b4a154dfd9374d6c5
AstafievAndrey <a.astafev@bars-open.ru>
2017-11-20 16:29:49 +0300

    block5_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    name:string = 'GeoPortalIndicatorDev';
+    pi:number = Math.PI;
+    date = new Date();
+    money:number = 350;
+    amount:number = 0.45;
+    object:any = {
+        foo:'bar',
+        baz:'qux',
+        nested:{
+            xyz:3,
+            numbers:[1,2,3]
+        }
+    };
 }
