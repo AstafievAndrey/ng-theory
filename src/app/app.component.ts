bc7268777969a06891652fa1952e06801e20d2a0
AstafievAndrey <a.astafev@bars-open.ru>
2017-12-12 11:07:20 +0300

    pipe async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    searchStr = '';
+    cars = [
+        {name:'Audi',desc:'description Audi'},
+        {name:'Bmv',desc:'description Bmv'},
+        {name:'Tesla',desc:'description Tesla'},
+        {name:'Toyota',desc:'description Toyota'},
+        {name:'Hyndai',desc:'description Hyndai'},
+        {name:'Honda',desc:'description Honda'},
+        {name:'Porche',desc:'description Porche'},
+    ];
+    second:number = 3000;
+
+    asyncTitle = Observable.of('Async title '+this.second+' second').delay(this.second);
 }
